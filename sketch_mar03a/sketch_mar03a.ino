b74d4760f3ddc7c7e2fb129829b0f3c64059bb1f
seeyonydnj1 <seeyon2@gmail.com>
2016-03-03 16:15:07 -0500

    cleaning up initial structure

---
+++ sketch_mar03a/sketch_mar03a.ino
@@ -0,0 +1,16 @@
+int led = 7;
+void setup() 
+{
+ //  initialize the digital pin as an output
+ pinMode(led,  OUTPUT); 
+
+}
+
+void loop() 
+{
+ digitalWrite(led, HIGH);  // turn the LED on (HIGH is the voltage level)
+ delay(1000);              // wait for a second
+ digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+ delay(5000);               // wait five seconds
+ 
+}
